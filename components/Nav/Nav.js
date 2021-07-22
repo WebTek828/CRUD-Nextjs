d514d1f477ea3efa8d981404e5d03b2dd59f764a
webtek <webtekprogrammer@gmail.com>
2021-07-22 18:32:11 +0630

    adds style to active nav link

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -2,10 +2,15 @@ import { useContext } from "react";
 import Link from "next/link";
 import styles from "./nav.module.css";
 import { MyContext } from "../../context/authContext";
+import { useRouter } from "next/router";
 
 import UserProfile from "./UserProfile/UserProfile";
 
 const Nav = (props) => {
+  const router = useRouter();
+  const activeNavLink = (path) =>
+    router.pathname === path ? styles.activeNavLink : "";
+
   const context = useContext(MyContext);
   const { token: isAuthenticated } = context.curUser;
 
@@ -14,19 +19,19 @@ const Nav = (props) => {
       <h1>Logo</h1>
       <ul className={styles.nav__lists}>
         <Link href="/">
-          <li>Home</li>
+          <li className={activeNavLink("/")}>Home</li>
         </Link>
         <Link href="/posts">
-          <li>Posts</li>
+          <li className={activeNavLink("/posts")}>Posts</li>
         </Link>
         {!isAuthenticated && (
           <Link href="/login">
-            <li>Login</li>
+            <li className={activeNavLink("/login")}>Login</li>
           </Link>
         )}
         {!isAuthenticated && (
           <Link href="/login">
-            <li>Register</li>
+            <li className={activeNavLink("/login")}>Register</li>
           </Link>
         )}
         {isAuthenticated && <UserProfile curUser={context.curUser} />}
