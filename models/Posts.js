f7a3d44618969a4e24d1de62d80b948cbdad6613
webtek <webtekprogrammer@gmail.com>
2021-07-22 13:20:45 +0630

    post can be created

--- models/Posts.js
+++ models/Posts.js
@@ -14,6 +14,10 @@ const postsSchema = new mongoose.Schema({
       type: String,
       required: true,
     },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   comments: [
--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -5,16 +5,16 @@ import Posts from "../../../models/Posts";
 const handler = async (req, res) => {
   connectDB();
   if (req.method === "POST") {
-    const { username, title, description } = req.body;
+    const { title, description, creator } = req.body;
     console.log(req.body);
-
     const newPost = await Posts.create({
-      creator: {
-        username,
-      },
+      creator,
       title,
       description,
+      likes: [],
+      comments: [],
     });
+    console.log(newPost);
     res.status(200).json({ newPost });
   } else if (req.method === "GET") {
     const allPosts = await Posts.find({});
