cd757e2d55c7cc6ccea7bd1257a1cbd2b7626706
webtek <webtekprogrammer@gmail.com>
2021-07-28 21:18:26 +0630

    post like users can be fetched by clicking like button

--- components/PostsPage/Posts/LikeDisplay/LikeDisplay.js
+++ components/PostsPage/Posts/LikeDisplay/LikeDisplay.js
@@ -1,12 +1,17 @@
 import styles from "./likeDisplay.module.css";
 
 const LikeDisplay = (props) => {
-  console.log(props.post);
-  const displayLikeUserHandler = () => {
+  const { post } = props;
+
+  const displayLikeUserHandler = async () => {
     console.log("Display like.");
+    const resp = await fetch(
+      `http://localhost:3000/api/posts/${post._id}/like`
+    );
+    const json = await resp.json();
+    console.log(json);
   };
 
-  const { post } = props;
   return (
     <>
       <span onClick={displayLikeUserHandler} className={styles.postLikes}>
