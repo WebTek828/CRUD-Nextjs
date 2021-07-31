cd2859b571190c3fb2d0adda0ab38c9cb0594e69
webtek <webtekprogrammer@gmail.com>
2021-07-31 15:54:48 +0630

    user can follow other users backend finishes

--- pages/api/follow/index.js
+++ pages/api/follow/index.js
@@ -1,23 +1,31 @@
 const Users = require("../../../models/Users");
 
+function follow(obj, followType, uid) {
+  const alreadyFollowing = obj[followType].filter(
+    (id) => id.toString() === uid
+  );
+
+  if (alreadyFollowing.length > 0) {
+    const updatedFollow = obj[followType].filter((id) => id.toString() !== uid);
+    obj[followType] = updatedFollow;
+  } else {
+    obj[followType].push(uid);
+  }
+}
+
 const handler = async (req, res) => {
   if (req.method === "POST") {
     try {
       const { followerUserId, userId } = req.body;
-      let follower = await Users.findById(followerUserId);
-      const alreadyFollowing = follower.following.filter(
-        (id) => id.toString() === userId
-      );
-      if (alreadyFollowing.length > 0) {
-        const updatedFollower = follower.following.filter(
-          (id) => id.toString() !== userId
-        );
-        follower.following = updatedFollower;
-      } else {
-        follower.following.push(userId);
-      }
+
+      const follower = await Users.findById(followerUserId);
+      const beingFollowedUser = await Users.findById(userId);
+
+      follow(follower, "following", userId);
+      follow(beingFollowedUser, "followers", followerUserId);
       await follower.save();
-      res.status(200).json(follower.following);
+      await beingFollowedUser.save();
+      res.status(200).json("Success");
     } catch (err) {
       console.log(err);
     }
